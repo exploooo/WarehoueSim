8f5871190f496fffa1f3d66c8f51bc233831ce20
unknown <exploooo>
2022-12-06 00:11:19 +0100

    Replace Rack Array with a Dynamic Array and added options of Adding more Rack Shelve's space, Adding a Rack Shelf Object to an Array, and deleting last Rack Shelf in Array

--- StorageSimulator/StorageSimulator.cpp
+++ StorageSimulator/StorageSimulator.cpp
@@ -22,17 +22,23 @@ using namespace std;
 // Optional maybe learn pointers and do some magic
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+// TBDIF
+// A storage Unit inside of a Rack
 class Unit {
 private:
 
 public:
 };
 
+// TBDIF
+// A class that represents a basic Rack
 class Rack {
 private:
     string RackID;
 
 public:
+
+    friend ostream& operator<<(ostream& os, Rack item);
 
     //Default Constructor
     Rack()
@@ -45,6 +51,8 @@ protected:
     int UnitsCount = 0;
 };
 
+
+// A class that represents as you guessed it.. a standard 3D Warehouse building
 class Warehouse {
 private:
     // Basic Wareshouse structural dimensions
@@ -53,8 +61,8 @@ private:
     float Width;
 
     // Warehouse's Racks
-    static const int MAX_NUMBER_OF_RACKS = 10;
-    Rack Racks[MAX_NUMBER_OF_RACKS];
+    int RackArrSize;
+    Rack* Racks;
 
 public:
 
@@ -63,42 +71,111 @@ public:
         return Length * Width * Height;
     }
     
-    // TBDIF - 10 for now
-    // Calculates Available rack spots basing on space
-    int calculateAvailableRackSpots(float space=0) {
-        return MAX_NUMBER_OF_RACKS - RackCounter;
+    // Returns dynamical value of Racks Array
+    void returnAvaiableRacks() {
+        cout << "Avaiable Racks: " << RackArrSize-RackCounter << endl;
+    }
+
+    // Dynamically extends Rack Table
+    void addMoreRacks(int rackSpacesToAdd) {
+
+        int newSize = RackArrSize + rackSpacesToAdd;
+
+        if (Racks != nullptr) {
+
+            Rack* tempRacks = new Rack[newSize];
+
+            for (int i = 0; i < RackArrSize; i++) {
+                tempRacks[i] = Racks[i]; // Tbh i don't know what that error means at the moment
+                                         //TBDIF Address that error
+            }
+
+            delete[] Racks;
+
+            Racks = new Rack[newSize];
+
+            for (int i = 0; i < RackArrSize; i++) {
+                Racks[i] = tempRacks[i];// Tbh i don't know what that error means at the moment
+                                         //TBDIF Address that error
+            }
+
+            delete[] tempRacks;
+
+        } else {
+            Racks = new Rack[newSize];
+        }
+
+        RackArrSize = newSize;
     }
 
     // Adds a new rack shelf to the warehouse
     void addRack(Rack rackToBeAdded) {
-        Racks[RackCounter] = rackToBeAdded;
-        RackCounter++;
+        if (RackCounter < RackArrSize) {
+            Racks[RackCounter] = rackToBeAdded;
+            RackCounter++;
+        } else {
+            cout << "Rack Spaces depleated! Add another one? (Y/N)" << endl;
+        }
     }
 
-    // Removes selected Rack Shelf
-    void removeRack(Rack rackToBeRemoved) {
-        Racks[RackCounter] = Rack();
-        RackCounter--;
+    // Removes last Rack shelf
+    void removeRack() {
+        if (RackCounter > 0) {
+            Racks[RackCounter-1] = Rack();
+            RackCounter--;
+        } else {
+            cout << "Rack Array is already empty! Cannot delete non-existent object!" << endl;
+        }
     }
 
-    // Creates a 3D square'ish storage compartment
-    Warehouse(float length, float width, float height) : Length(length), Width(width), Height(height) 
-    {};
+    // Displays all Racks in Warehouse
+    void displayRacks() {
+        cout << "Racks: ";
+        for (int i = 0; i < RackCounter; i++) {
+            if (i != RackCounter - 1) {
+                cout << Racks[i] << ", ";
+            } else {
+                cout << Racks[i] << endl;
+            }
+        }
+    }
+
+    // Creates a 3D square'ish storage compartment with Racks allready
+    Warehouse(float length, float width, float height, int rackArrSize) : Length(length), Width(width), Height(height), RackArrSize(rackArrSize)
+    {
+        Racks = new Rack[rackArrSize];
+    };
+
+    // Creates a 3D square'ish storage compartment without Racks
+    Warehouse(float length, float width, float height) : Length(length), Width(width), Height(height)
+    {
+        Racks = nullptr;
+        RackArrSize = 0;
+    };
+
+    // Destructor that also deletes dynamicly allocated memory *CRUCIAL DO NOT DELETE* - helps avoid memory leak 
+    ~Warehouse() {
+        delete [] Racks;
+    }
 
 protected:
-    int RackCounter = 0;
+    int RackCounter=0;
 };
+
+ostream& operator<<(ostream& os, Rack item) {
+    os << item.RackID;
+    return os;
+}
 
 int main()
 {
+    // Main is used mainly for tests at the moment
+
     Rack rack_a = Rack("#A1", 10);
+    Rack rack_b = Rack("#A2", 20);
+    Rack rack_c = Rack("#B1", 30);
     Warehouse storage_a = Warehouse(110, 300, 12);
-
-    cout << storage_a.calculateSpace() << endl;
-    cout << storage_a.calculateAvailableRackSpots() << endl;
-    
-    storage_a.addRack(rack_a);
-
-    cout << storage_a.calculateAvailableRackSpots() << endl;
+   
+    return 0;
 }
 
